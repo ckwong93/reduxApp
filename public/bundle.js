e7987991839ba382d152962b6729b0694b943841
Calvin Kwong <ckwong93@gmail.com>
2017-12-26 16:22:15 -0800

    use spread operator in reducer

--- public/bundle.js
+++ public/bundle.js
@@ -72,17 +72,18 @@
 
 var _redux = __webpack_require__(8);
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 // STEP 3 define reducers
 var reducer = function reducer() {
-  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : { books: [] };
   var action = arguments[1];
 
   switch (action.type) {
     case "POST_BOOK":
-      return state = action.payload;
-      break;
-    case "DECREMENT":
-      return state - action.payload;
+      // let books = state.books.concat(action.payload);
+      // return {books};
+      return { books: [].concat(_toConsumableArray(state.books), _toConsumableArray(action.payload)) };
       break;
   }
   return state;
@@ -92,7 +93,7 @@ var store = (0, _redux.createStore)(reducer);
 
 store.subscribe(function () {
   console.log('current state is ', store.getState());
-  console.log('current state is ', store.getState()[1].price);
+  // console.log('current state is ', store.getState()[1].price)
 });
 // STEP 2 create and dispatch actions
 
@@ -109,6 +110,16 @@ store.dispatch({
     title: 'this is the 2nd book title',
     price: '870',
     description: 'this is the 2nd desc'
+  }]
+});
+
+store.dispatch({
+  type: "POST_BOOK",
+  payload: [{
+    id: 3,
+    title: 'this is the 3rd book title',
+    price: '3.333',
+    description: 'this is the desc'
   }]
 });
 
--- src/app.js
+++ src/app.js
@@ -2,13 +2,12 @@
 import {createStore} from 'redux';
 
 // STEP 3 define reducers
-const reducer = function(state=[],action){
+const reducer = function(state={books: []},action){
   switch(action.type){
     case "POST_BOOK":
-    return state = action.payload;
-    break;
-    case "DECREMENT":
-    return state - action.payload;
+    // let books = state.books.concat(action.payload);
+    // return {books};
+    return {books: [...state.books,...action.payload]}
     break;
   }
   return state
@@ -18,7 +17,7 @@ const store = createStore(reducer);
 
 store.subscribe(function(){
   console.log('current state is ', store.getState());
-  console.log('current state is ', store.getState()[1].price)
+  // console.log('current state is ', store.getState()[1].price)
 })
 // STEP 2 create and dispatch actions
 
@@ -37,6 +36,18 @@ store.dispatch({
   title: 'this is the 2nd book title',
   price: '870',
   description: 'this is the 2nd desc'
+  }
+  ]
+});
+
+store.dispatch({
+  type:"POST_BOOK",
+  payload:
+  [{
+  id: 3,
+  title: 'this is the 3rd book title',
+  price: '3.333',
+  description: 'this is the desc'
   }
   ]
 });
