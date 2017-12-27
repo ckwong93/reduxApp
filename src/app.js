94027618d7fc805fec14d185d659be508c3af6fd
Calvin Kwong <ckwong93@gmail.com>
2017-12-27 10:00:21 -0800

    create react component BookList...create provider in app.js to connect redux state

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
 "use strict"
+
+//REACT
+import React from 'react';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+
+
+// middleware
 import {applyMiddleware,createStore} from 'redux';
 import logger from 'redux-logger';
 
@@ -18,6 +26,19 @@ const store = createStore(reducers, middleware);
 // store.subscribe(function(){
 //   console.log('current state is ', store.getState());
 // })
+
+import BooksList from './components/pages/bookslist';
+render(
+  <Provider store={store}>
+    <BooksList />
+  </Provider>, document.getElementById('app')
+);
+
+
+
+
+
+
 
 
 // STEP 2 create and dispatch actions
---
+++ src/components/pages/booksList.js
@@ -0,0 +1,17 @@
+"use strict"
+
+import React from 'react';
+
+class BooksList extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>
+          Hello React - BooksList Component
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default BooksList;
